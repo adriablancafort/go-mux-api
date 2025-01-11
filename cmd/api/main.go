d964e84be871f43078c66e6d02a8ed3dce1ea039
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-11 15:18:04 +0100

    error handling

---
+++ cmd/api/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /products/{id}", getProduct)
+
+	http.ListenAndServe(":8080", mux)
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+    id := r.URL.Path[len("/products/"):]
+
+    w.Header().Set("Content-Type", "application/json")
+
+    product := map[string]interface{}{
+        "id":   id,
+        "name": "Product " + id,
+    }
+
+    err := json.NewEncoder(w).Encode(product)
+	if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.WriteHeader(http.StatusOK)
+}
