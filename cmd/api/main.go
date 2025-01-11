eba80d9f8d546ab4224f8069d70028adc1741954
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-11 17:33:18 +0100

    post product

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,26 +5,48 @@ import (
 	"net/http"
 )
 
+type Product struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Price float64 `json:"price"`
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /products/{id}", getProduct)
+	mux.HandleFunc("POST /products", postProduct)
 
 	http.ListenAndServe(":8080", mux)
 }
 
 func getProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	
     id := r.PathValue("id")
 
-    w.Header().Set("Content-Type", "application/json")
-
-    product := map[string]interface{}{
-        "id":   id,
-        "name": "Product " + id,
+	product := Product{
+        ID:    id,
+        Name:  "iPhone 12",	
+        Price: 899.95,
     }
 
-	err := json.NewEncoder(w).Encode(product); 
-	if err != nil {
+	if err := json.NewEncoder(w).Encode(product); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+}
+
+func postProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	
+    var product Product
+    if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    if err := json.NewEncoder(w).Encode(product); err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
