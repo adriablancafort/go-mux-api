5010e2a5d263dd1c993b2dd0005f3dcfbe8ed301
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-12 03:21:59 +0100

    query db

--- internal/api/products/handlers.go
+++ internal/api/products/handlers.go
@@ -3,20 +3,33 @@ package products
 import (
     "encoding/json"
     "net/http"
+    "log"
+
+    "github.com/adriablancafort/go-mux-api/internal/api/database"
 )
 
 func getProduct(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     
-    id := r.URL.Query().Get("id")
-
-    product := Product{
-        ID:    id,
-        Name:  "iPhone 12",	
-        Price: 899.95,
+    id := r.PathValue("id")
+    if id == "" {
+        http.Error(w, "Missing product ID", http.StatusBadRequest)
+        return
     }
 
-    if err := json.NewEncoder(w).Encode(product); err != nil {
+    var err error
+    var product Product
+    query := `SELECT id, name, price FROM products WHERE id = $1`
+    row := database.DB.QueryRow(query, id)
+    err = row.Scan(&product.ID, &product.Name, &product.Price)
+    if err != nil {
+        log.Println("Error fetching product:", err)
+        http.Error(w, "Product not found", http.StatusNotFound)
+        return
+    }
+
+    err = json.NewEncoder(w).Encode(product)
+    if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
@@ -28,6 +41,14 @@ func postProduct(w http.ResponseWriter, r *http.Request) {
     var product Product
     if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
         http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    query := `INSERT INTO products (id, name, price) VALUES ($1, $2, $3)`
+    _, err := database.DB.Exec(query, product.ID, product.Name, product.Price)
+    if err != nil {
+        log.Println("Error inserting product:", err)
+        http.Error(w, "Error inserting product", http.StatusInternalServerError)
         return
     }
 
