afc48d51d6ea58dc1bd9a554295bc03da1e3340a
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-12 16:16:18 +0100

    no trailing slash at the end of paths

--- internal/api/products/handlers.go
+++ internal/api/products/handlers.go
@@ -6,7 +6,7 @@ import (
 )
 
 func RegisterRoutes(mux *http.ServeMux) {
-    mux.HandleFunc("GET /products/", getProducts)
+    mux.HandleFunc("GET /products", getProducts)
     mux.HandleFunc("GET /products/{id}", getProduct)
     mux.HandleFunc("POST /products", postProduct)
 }
